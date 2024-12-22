6ccda669e8fc110abf2c8dc6726d5db3ca3915ef
ThakurSadguru <sadgurusinghthakur@gmail.com>
2024-12-22 13:26:50 +0530

    Created Header

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,11 @@ const App = () => {
   return (
     <div className='app'>
       <Navbar/>
-      <Routes/>
+      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path = '/cart' element={<Cart/>}/>
       <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
