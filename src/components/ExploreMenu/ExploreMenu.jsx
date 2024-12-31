ecf558da08e8948358fb9fa1cb6e2336fdf3f95d
ThakurSadguru <sadgurusinghthakur@gmail.com>
2024-12-31 17:05:02 +0530

    ExploreMenu.css completed

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,9 +5,9 @@ const ExploreMenu = () => {
   return (
     <div className='explore-menu' id ='explore-menu'>
       <h1>Explore our menu</h1>
-      <p>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your craving and elevate your dining experience,one delicious meal at a time.
       </p>
-      <div calssName="explore-menu-list">
+      <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
           return(
             <div key={index} className='explore-menu-list-item'>
@@ -17,7 +17,7 @@ const ExploreMenu = () => {
           )
         })}
       </div>
-      
+      <hr />
     </div>
   )
 }
