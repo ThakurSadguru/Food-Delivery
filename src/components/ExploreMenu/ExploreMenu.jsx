bbc6357a046f36df201d449113ff388c933204cb
ThakurSadguru <sadgurusinghthakur@gmail.com>
2024-12-31 16:26:21 +0530

    ExploreMenu.jsx created

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,19 @@ const ExploreMenu = () => {
   return (
     <div className='explore-menu' id ='explore-menu'>
       <h1>Explore our menu</h1>
-      <p>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to</p>
+      <p>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to
+      </p>
+      <div calssName="explore-menu-list">
+        {menu_list.map((item,index)=>{
+          return(
+            <div key={index} className='explore-menu-list-item'>
+              <img src={item.menu_image} alt=""/>
+              <p>{item.menu_name}</p>
+              </div>
+          )
+        })}
+      </div>
+      
     </div>
   )
 }
