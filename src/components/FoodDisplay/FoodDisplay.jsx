d232d26f9214112f42d3aa66cb037c12e6ce20dd
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-02 08:20:20 +0530

    took props from home.jsx into fooddisplay.jsx

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../Context/StoreContext';
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
 
   const {food_list}= useContext(StoreContext);
   return (
