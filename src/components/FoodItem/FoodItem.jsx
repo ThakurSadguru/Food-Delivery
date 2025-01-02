29598ce190123b01dcc454cfb96bd1f55d26924d
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-02 08:41:48 +0530

    div added for name  in foodItems.jsx

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './FoodItem.css'
+import { assets } from '../../assets/assets'
 const FoodItem = ({id,name,price,description,image}) => {
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt="" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-naming-rating">
+          <p>{name}</p>
+          <image src={assets.rating_starts} alt=""/>
+        </div>
       </div>
       
     </div>
