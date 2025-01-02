fd420a27ee02fb7a3f4fb158c6e61550556e3ade
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-02 08:38:04 +0530

    div added for image in foodItems.jsx

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import './FoodItem.css'
 const FoodItem = ({id,name,price,description,image}) => {
   return (
-    <div>
+    <div className='food-item'>
+      <div className="food-item-img-container">
+        <img className='food-item-image' src={image} alt="" />
+      </div>
       
     </div>
   )
