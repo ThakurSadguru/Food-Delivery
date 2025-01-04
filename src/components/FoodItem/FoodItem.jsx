33aa7c2244915f4a6a8932b4702665e5241968c7
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-05 00:09:29 +0530

    css to fooditem

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -8,7 +8,7 @@ const FoodItem = ({id,name,price,description,image}) => {
         <img className='food-item-image' src={image} alt="" />
       </div>
       <div className="food-item-info">
-        <div className="food-item-naming-rating">
+        <div className="food-item-name-rating">
           <p>{name}</p>
           <img src={assets.rating_starts} alt=""/>
         </div>
