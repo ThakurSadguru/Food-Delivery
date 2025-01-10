c90c20ec334d2be6ca59d1d91b69fe7c139ee1bd
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-10 18:31:36 +0530

    appdownload complete, responsive website

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './AppDownload.css'
+import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
-    <div>
-      
+    <div className='app-download' id='app-download'>
+      <p>For Better Experience<br/> Our App</p>
+      <div className="app-download-platform">
+        <img src={assets.play_store} alt=''/>
+        <img src={assets.app_store} alt=''/>
+      </div>
     </div>
   )
 }
